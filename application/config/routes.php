f91cb5e15a09633aef2ce5442187a9dc21fc5951
alimuddien <saa181@gmail.com>
2018-07-19 20:35:32 +0700

    Keterangan Route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+//ROUTES MODIFIKASI
 $route['default_controller'] = 'redirect';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
